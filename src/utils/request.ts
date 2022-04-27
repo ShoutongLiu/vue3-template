cd3cda42d8cb45b8b5ff72904640e78a496e424d
ShoutongLiu <1183063367@qq.com>
2022-04-27 15:17:17 +0800

    add husky

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,43 +1,37 @@
-import axios from "axios"
+import axios from 'axios'
 import type { AxiosInstance, AxiosRequestConfig } from 'axios'
-import { ElMessage } from 'element-plus'
-
 
 class Request {
   // axios 实例
   instance: AxiosInstance
 
-  constructor(config: AxiosRequestConfig) {
-    this.instance = axios.create(config)
+  constructor(configs: AxiosRequestConfig) {
+    this.instance = axios.create(configs)
 
-    //全局请求拦截
+    // 全局请求拦截
     this.instance.interceptors.request.use(
-      (config) => {
-        console.log(config)
+      (config: unknown) => {
         return config
       },
       (error) => {
         console.log('全局请求失败拦截', error)
       }
     )
-    //全局响应拦截
+    // 全局响应拦截
     this.instance.interceptors.response.use(
       (res) => {
-        //res为AxiosResponse类型，含有config\data\headers\request\status\statusText属性
-        console.log(res)
-        //改造返回的数据类型，即将AxiosResponse的data返回
+        // res为AxiosResponse类型，含有config\data\headers\request\status\statusText属性
+        // 改造返回的数据类型，即将AxiosResponse的data返回
         return res.data
       },
       (error) => {
-        console.log('全局响应失败拦截')
-        console.log(error.request)//
-        console.log(error.response)
         return error
       }
     )
   }
-  //返回的Promise中结果类型为AxiosResponse<any>
-  //加入泛型限定，返回数据类型为T，
+
+  // 返回的Promise中结果类型为AxiosResponse<any>
+  // 加入泛型限定，返回数据类型为T，
   request<T>(config: AxiosRequestConfig<T>): Promise<T> {
     return new Promise<T>((resolve, reject) => {
       this.instance
@@ -52,10 +46,10 @@ class Request {
   }
 }
 
-//使用环境配置
+// 使用环境配置
 const request = new Request({
   baseURL: process.env.VITE_APP_WEB_URL,
-  timeout: 10 * 1000,
+  timeout: 10 * 1000
 })
 
 export default request
