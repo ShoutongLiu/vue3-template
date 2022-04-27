7b5217cedee03fab66ad93bfd1815ab71a684cbd
ShoutongLiu <1183063367@qq.com>
2022-04-27 18:14:54 +0800

    feat(deps): tom update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,20 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+    es2021: true
   },
   extends: [
     'plugin:vue/essential',
     'airbnb-base',
-    'plugin:prettier/recommended', // 添加 prettier 插件
+    'plugin:prettier/recommended' // 添加 prettier 插件
   ],
   parserOptions: {
     ecmaVersion: 'latest',
     parser: '@typescript-eslint/parser',
-    sourceType: 'module',
+    sourceType: 'module'
   },
-  plugins: [
-    'vue',
-    '@typescript-eslint',
-  ],
+  plugins: ['vue', '@typescript-eslint'],
   rules: {
-  },
-};
+    'no-console': 'off'
+  }
+}
